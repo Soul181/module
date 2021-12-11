1d4b880db1048835908bbed53b4ab305f1559e64
Soul181 <s.solonetc@mail.ru>
2021-12-11 23:05:32 +0300

    add view component refactoring with league/plates

--- public/index.php
+++ public/index.php
@@ -2,12 +2,8 @@
 
 require '../vendor/autoload.php';
 
-if ($_SERVER['REQUEST_URI'] == '/home') {
-    require '../app/controllers/homepage.php';
-} else {
-    var_dump(404); exit;
-}
-exit;
+// Create new Plates instance
+$templates = new League\Plates\Engine('../app/views');
 
-
-
+// Render a template
+echo $templates->render('about', ['title' => 'MARK']);
