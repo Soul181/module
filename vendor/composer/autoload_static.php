b49886c0cfdfe9c979bdf7d8d01c50608e772ca8
Soul181 <s.solonetc@mail.ru>
2021-12-12 10:18:04 +0300

    add component fast-route

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,9 +8,15 @@ class ComposerStaticInit5c9c0d7a04ac72e32e1f9a138afb35b2
 {
     public static $files = array (
         '3917c79c5052b270641b5a200963dbc2' => __DIR__ . '/..' . '/kint-php/kint/init.php',
+        '6157b075b923803e5ef157aeb43b83bd' => __DIR__ . '/..' . '/tamtamchik/simple-flash/src/function.php',
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Tamtamchik\\SimpleFlash\\' => 23,
+        ),
         'L' => 
         array (
             'League\\Plates\\' => 14,
@@ -18,6 +24,10 @@ class ComposerStaticInit5c9c0d7a04ac72e32e1f9a138afb35b2
         'K' => 
         array (
             'Kint\\' => 5,
+        ),
+        'F' => 
+        array (
+            'FastRoute\\' => 10,
         ),
         'A' => 
         array (
@@ -27,6 +37,10 @@ class ComposerStaticInit5c9c0d7a04ac72e32e1f9a138afb35b2
     );
 
     public static $prefixDirsPsr4 = array (
+        'Tamtamchik\\SimpleFlash\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tamtamchik/simple-flash/src',
+        ),
         'League\\Plates\\' => 
         array (
             0 => __DIR__ . '/..' . '/league/plates/src',
@@ -34,6 +48,10 @@ class ComposerStaticInit5c9c0d7a04ac72e32e1f9a138afb35b2
         'Kint\\' => 
         array (
             0 => __DIR__ . '/..' . '/kint-php/kint/src',
+        ),
+        'FastRoute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nikic/fast-route/src',
         ),
         'Aura\\SqlQuery\\' => 
         array (
